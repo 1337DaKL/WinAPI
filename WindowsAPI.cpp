fd22d3c8668df0620f4a1f226b5cd24ec136514c
1337DaKL <luongtrinh2k3ndad@gmail.com>
2025-05-19 22:17:20 +0700

    bai 5

--- WindowsAPI.cpp
+++ WindowsAPI.cpp
@@ -3,6 +3,9 @@
 #include<Windows.h>
 #include "WindowsAPI.h"
 #include<string>
+#include <sstream>
+#include<map>
+
 using namespace std;
 //### ** Bài 2: Đọc tên người dùng và chào lại * *
 //
@@ -124,131 +127,184 @@ using namespace std;
 //
 //- Người dùng chọn mục bằng nhập số
 //- Thực hiện hành động tương ứng
+//int main() {
+//	HANDLE h_in = GetStdHandle(STD_INPUT_HANDLE);
+//	HANDLE h_out = GetStdHandle(STD_OUTPUT_HANDLE);
+//	char messenge[] = "1. Tinh tong\n2.Tinh hieu\n3.Thoat\nNhap chuc nang ban mong muon: ";
+//	DWORD ki_tu_in_ra;
+//	WriteConsoleA(
+//		h_out,
+//		messenge,
+//		(DWORD)strlen(messenge),
+//		&ki_tu_in_ra,
+//		NULL
+//	);
+//	char input[100];
+//	DWORD ki_tu_nhap_vao;
+//	ReadConsoleA(
+//		h_in,
+//		input,
+//		100,
+//		&ki_tu_nhap_vao,
+//		NULL
+//	);
+//	int nhap = atoi(input);
+//	if (nhap == 1) {
+//		char notion_sum[] = "Thuc hien chuc nang tinh tong a + b.\nNhap a: ";
+//		WriteConsoleA(
+//			h_out,
+//			notion_sum,
+//			(DWORD)strlen(notion_sum),
+//			&ki_tu_in_ra,
+//			NULL
+//		);
+//		char a[100];
+//		ReadConsoleA(
+//			h_in,
+//			a,
+//			100,
+//			&ki_tu_nhap_vao,
+//			NULL
+//		);
+//		char notion_input_b[] = "Nhap b: ";
+//		WriteConsoleA(
+//			h_out,
+//			notion_input_b,
+//			(DWORD)strlen(notion_input_b),
+//			&ki_tu_in_ra,
+//			NULL
+//		);
+//		char b[100];
+//		ReadConsoleA(
+//			h_in,
+//			b,
+//			100,
+//			&ki_tu_nhap_vao,
+//			NULL
+//		);
+//		int tong = atoi(a) + atoi(b);
+//		string results = "Ket qua phep tinh a + b la: " + to_string(tong);
+//		WriteConsoleA(
+//			h_out,
+//			results.c_str(),
+//			(DWORD)size(results),
+//			&ki_tu_in_ra,
+//			NULL
+//		);
+//	}
+//	else if (nhap == 2) {
+//		char notion_tru[] = "Thuc hien chuc nang tinh hieu a - b.\nNhap a: ";
+//		WriteConsoleA(
+//			h_out,
+//			notion_tru,
+//			(DWORD)strlen(notion_tru),
+//			&ki_tu_in_ra,
+//			NULL
+//		);
+//		char a[100];
+//		ReadConsoleA(
+//			h_in,
+//			a,
+//			100,
+//			&ki_tu_nhap_vao,
+//			NULL
+//		);
+//		char notion_b[] = "Nhap b: ";
+//		WriteConsoleA(
+//			h_out,
+//			notion_b,
+//			(DWORD)strlen(notion_b),
+//			&ki_tu_in_ra,
+//			NULL
+//		);
+//		char b[100];
+//		ReadConsoleA(
+//			h_in,
+//			b,
+//			100,
+//			&ki_tu_nhap_vao,
+//			NULL
+//		);
+//		int results = atoi(a) - atoi(b);
+//		string  out = "Ket qua phep tinh a - b la: " + to_string(results);
+//		WriteConsoleA(
+//			h_out,
+//			out.c_str(),
+//			(DWORD)size(out),
+//			&ki_tu_in_ra,
+//			NULL
+//		);
+//	}
+//	else if (nhap == 3) {
+//		char notion[] = "Thoat chuong trinh thanh cong \n";
+//		WriteConsoleA(
+//			h_out,
+//			notion,
+//			(DWORD)strlen(notion),
+//			&ki_tu_in_ra,
+//			NULL
+//		);
+//	}
+//	else {
+//		char error[] = "Vui long nhap mot trong cac chuc nang co ben tren. Khoi dong lai chuong trinh di";
+//		WriteConsoleA(
+//			h_in,
+//			error,
+//			(DWORD)strlen(error),
+//			&ki_tu_in_ra,
+//			NULL
+//		);
+//	}
+//}
+//### ** Bài 5: Đếm số từ nhập vào * *
+//
+//-Nhập một dòng văn bản
+//- Đếm số từ trong chuỗi đó(tách bằng khoảng trắng)
+//- In ra kết quả
 int main() {
 	HANDLE h_in = GetStdHandle(STD_INPUT_HANDLE);
 	HANDLE h_out = GetStdHandle(STD_OUTPUT_HANDLE);
-	char messenge[] = "1. Tinh tong\n2.Tinh hieu\n3.Thoat\nNhap chuc nang ban mong muon: ";
-	DWORD ki_tu_in_ra;
+	DWORD real_in;
+	DWORD real_out;
+	char notion[] = "Nhap chuoi muon dem: ";
 	WriteConsoleA(
 		h_out,
-		messenge,
-		(DWORD)strlen(messenge),
-		&ki_tu_in_ra,
+		notion,
+		(DWORD)strlen(notion),
+		&real_out,
 		NULL
 	);
-	char input[100];
-	DWORD ki_tu_nhap_vao;
+	char input[100] = {0};
 	ReadConsoleA(
 		h_in,
 		input,
 		100,
-		&ki_tu_nhap_vao,
+		&real_in,
 		NULL
 	);
-	int nhap = atoi(input);
-	if (nhap == 1) {
-		char notion_sum[] = "Thuc hien chuc nang tinh tong a + b.\nNhap a: ";
-		WriteConsoleA(
-			h_out,
-			notion_sum,
-			(DWORD)strlen(notion_sum),
-			&ki_tu_in_ra,
-			NULL
-		);
-		char a[100];
-		ReadConsoleA(
-			h_in,
-			a,
-			100,
-			&ki_tu_nhap_vao,
-			NULL
-		);
-		char notion_input_b[] = "Nhap b: ";
-		WriteConsoleA(
-			h_out,
-			notion_input_b,
-			(DWORD)strlen(notion_input_b),
-			&ki_tu_in_ra,
-			NULL
-		);
-		char b[100];
-		ReadConsoleA(
-			h_in,
-			b,
-			100,
-			&ki_tu_nhap_vao,
-			NULL
-		);
-		int tong = atoi(a) + atoi(b);
-		string results = "Ket qua phep tinh a + b la: " + to_string(tong);
+	if (size(input)) {
+		input[real_in] = '\0';
+	}
+	
+	string string_input = string(input);
+	stringstream ss(string_input);
+	string word;
+	map<string, int> word_count;
+	while (ss >> word) {
+		if (word_count.count(word) != 0) {
+			word_count[word] += 1;
+		}
+		else {
+			word_count[word] = 1;
+		}
+	}
+	for (auto it : word_count){
+		string results = "Key: " + it.first + " Value: " + to_string(it.second) + "\n";
 		WriteConsoleA(
 			h_out,
 			results.c_str(),
 			(DWORD)size(results),
-			&ki_tu_in_ra,
-			NULL
-		);
-	}
-	else if (nhap == 2) {
-		char notion_tru[] = "Thuc hien chuc nang tinh hieu a - b.\nNhap a: ";
-		WriteConsoleA(
-			h_out,
-			notion_tru,
-			(DWORD)strlen(notion_tru),
-			&ki_tu_in_ra,
-			NULL
-		);
-		char a[100];
-		ReadConsoleA(
-			h_in,
-			a,
-			100,
-			&ki_tu_nhap_vao,
-			NULL
-		);
-		char notion_b[] = "Nhap b: ";
-		WriteConsoleA(
-			h_out,
-			notion_b,
-			(DWORD)strlen(notion_b),
-			&ki_tu_in_ra,
-			NULL
-		);
-		char b[100];
-		ReadConsoleA(
-			h_in,
-			b,
-			100,
-			&ki_tu_nhap_vao,
-			NULL
-		);
-		int results = atoi(a) - atoi(b);
-		string  out = "Ket qua phep tinh a - b la: " + to_string(results);
-		WriteConsoleA(
-			h_out,
-			out.c_str(),
-			(DWORD)size(out),
-			&ki_tu_in_ra,
-			NULL
-		);
-	}
-	else if (nhap == 3) {
-		char notion[] = "Thoat chuong trinh thanh cong \n";
-		WriteConsoleA(
-			h_out,
-			notion,
-			(DWORD)strlen(notion),
-			&ki_tu_in_ra,
-			NULL
-		);
-	}
-	else {
-		char error[] = "Vui long nhap mot trong cac chuc nang co ben tren. Khoi dong lai chuong trinh di";
-		WriteConsoleA(
-			h_in,
-			error,
-			(DWORD)strlen(error),
-			&ki_tu_in_ra,
+			&real_out,
 			NULL
 		);
 	}
